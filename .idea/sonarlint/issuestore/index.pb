8b829c64e780dff5fd3240cf92045f084350ca82
ec-ayrton <ec.ayrton@gmail.com>
2023-03-12 21:51:23 -0300

    Remoção das referencias a entidade Produto

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,13 +5,5 @@ A
 package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-;
-knexfile.js,2/7/27364759f521fe7be16cb20ecbc5ef80335117b2
-B
-docker-compose.yml,3/5/35b8c13cf2eb2a194eada000eb310d65aed53b2a
 C
 src/errorHandler.js,1/0/10c792494db520625ef63e6da0ca039aae8b44df
-6
-app.js,4/a/4a68969ef8e858229267b842dedf42ab5dde4d50
-Q
-!src/validators/PessoaValidator.js,c/4/c466e53b319fe460f1bbf58d703c2721c901810d
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const PessoaController = require('../controllers/pessoaController');
-const ProdutoController = require('../controllers/produtoController');
 
 
 router.get('/pessoas', PessoaController.listarTodasPessoas);
@@ -9,6 +8,4 @@ router.post('/pessoas', PessoaController.cadastrar);
 router.delete('/pessoas/:id', PessoaController.removerPessoa);
 router.put('/pessoas/:id',PessoaController.editarPessoa)
 
-router.get('/produtos', ProdutoController.listarTodosProdutos);
-router.post('/produtos', ProdutoController.cadastrar);
 module.exports = router;
